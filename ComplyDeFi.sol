1150defa4237d98b20a98a435ea1f1af876e6067
Birdnavas <birdnavas@live.com>
2024-07-22 16:24:35 -0600

    1.0

---
+++ ComplyDeFi.sol
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./IComplyDeFi.sol";
+import "./factory/IIdFactory.sol";
+import "COMPLYDEFI/openzep/Ownable.sol";
+import "ONCHAINID/interface/IClaimIssuer.sol";
+
+
+contract ComplyDeFi is IComplyDeFi, Ownable {
+
+    /// address of the ONCHAINID Factory contract
+    address public factory;
+    /// ONCHAINID linked to the complyDeFi contract
+    address public complyDeFiID;
+
+    /// claims required to validate a pattern
+    uint[] public requiredClaims;
+    /// correspondence table linking trusted issuers and the claim topics they are trusted for
+    mapping(address => uint[]) public trustedIssuerClaims;
+
+
+
+    // modifier checking the compliance status of the wallet calling the transaction
+    // this modifier has to be put on each function that requires a compliance check
+    modifier onlyComply() {
+        require(isComply(msg.sender), 'ComplyDeFi : caller not compliant');
+        _;
+    }
+
+    /**
+     *  @dev Constructor of the ComplyDeFi contract
+     *  insert this constructor in the constructor of your DeFi app inheriting ComplyDeFi
+     *  @param _factory the address of the ONCHAINID factory
+     *  @param _id the address of the ONCHAINID of the DeFi protocol implementing ComplyDeFi
+     */
+    constructor (address _factory, address _id) {
+        factory = _factory;
+        complyDeFiID = _id;
+    }
+
+    /**
+     *  @dev See {IComplyDeFi-setFactory}.
+     */
+    function setFactory(address _factory) external override onlyOwner {
+        factory = _factory;
+        emit FactorySet(_factory);
+    }
+
+    /**
+     *  @dev See {IComplyDeFi-isTrustedIssuer}.
+     */
+    function isTrustedIssuer(address _issuer) public view override returns (bool){
+        if (trustedIssuerClaims[_issuer].length > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *  @dev See {IComplyDeFi-addTrustedIssuer}.
+     */
+    function addTrustedIssuer(address _issuer, uint[] memory _claimTopics) external override onlyOwner {
+        require(!isTrustedIssuer(_issuer), "issuer already exists");
+        trustedIssuerClaims[_issuer] = _claimTopics;
+        emit TrustedIssuerAdded(_issuer, _claimTopics);
+    }
+
+    /**
+     *  @dev See {IComplyDeFi-removeTrustedIssuer}.
+     */
+    function removeTrustedIssuer(address _issuer) external override onlyOwner {
+        require(isTrustedIssuer(_issuer), "issuer doesn't exists");
+        delete trustedIssuerClaims[_issuer];
+        emit TrustedIssuerRemoved(_issuer);
+    }
+
+    /**
+     *  @dev See {IComplyDeFi-updateIssuerTopics}.
+     */
+    function updateIssuerTopics(address _issuer, uint[] memory _claimTopics) external override onlyOwner {
+        require(isTrustedIssuer(_issuer), "issuer doesn't exists");
+        require(_claimTopics.length > 0 , "claim set cannot be empty");
+        trustedIssuerClaims[_issuer] = _claimTopics;
+        emit TrustedIssuerUpdated(_issuer, _claimTopics);
+    }
+
+    /**
+     *  @dev See {IComplyDeFi-isClaimRequired}.
+     */
+    function isClaimRequired(uint _claim) public view override returns (bool) {
+        uint length = requiredClaims.length;
+        for (uint i = 0; i < length; i++) {
+            if (requiredClaims[i] == _claim) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *  @dev See {IComplyDeFi-addRequiredClaim}.
+     */
+    function addRequiredClaim(uint _claim) external override onlyOwner {
+        require(!isClaimRequired(_claim), "claim already in the list of required claims");
+        requiredClaims.push(_claim);
+        emit ClaimRequired(_claim);
+    }
+
+    /**
+     *  @dev See {IComplyDeFi-removeRequiredClaim}.
+     */
+    function removeRequiredClaim(uint _claim) external override onlyOwner {
+        require(isClaimRequired(_claim), "claim already in the list of required claims");
+        uint length = requiredClaims.length;
+        for (uint i = 0; i < length; i++) {
+            if (requiredClaims[i] == _claim) {
+                requiredClaims[i] = requiredClaims[length - 1];
+                requiredClaims.pop();
+                emit ClaimUnRequired(_claim);
+                break;
+            }
+        }
+
+    }
+
+    /**
+     *  @dev See {IComplyDeFi-hasClaimTopic}.
+     */
+    function hasClaimTopic(address _issuer, uint _claimTopic) public view override returns (bool) {
+        uint length = trustedIssuerClaims[_issuer].length;
+        uint[] memory claimTopics = trustedIssuerClaims[_issuer];
+        for (uint i = 0; i < length; i++) {
+            if (claimTopics[i] == _claimTopic) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *  @dev See {IComplyDeFi-isComply}.
+     */
+    function isComply(address _user) public view override returns (bool) {
+        address _identity = (IIdFactory(factory)).getIdentity(_user);
+        // check that the identity was deployed by the factory, other identities are not allowed
+        require(_identity != address(0), 'invalid identity contract');
+        // claim variables
+        uint256 foundClaimTopic;
+        uint256 scheme;
+        address issuer;
+        bytes memory sig;
+        bytes memory data;
+        uint256 claimTopic;
+        // for loop on the claims required by the pattern
+        for (claimTopic = 0; claimTopic < requiredClaims.length; claimTopic++) {
+            // fetch claim IDs held by the identity
+            bytes32[] memory claimIds = (IIdentity(_identity)).getClaimIdsByTopic(requiredClaims[claimTopic]);
+            // if identity is not containing any claim, check is returning false
+            if (claimIds.length == 0) {
+                return false;
+            }
+            // for loop on the claims held by the identity to compare with the required claim currently checked
+            for (uint256 j = 0; j < claimIds.length; j++) {
+                (foundClaimTopic, scheme, issuer, sig, data, ) = (IIdentity(_identity)).getClaim(claimIds[j]);
+
+                try IClaimIssuer(issuer).isClaimValid(IIdentity(_identity), requiredClaims[claimTopic], sig,
+                    data) returns(bool _validity){
+                    if (
+                        _validity
+                        && hasClaimTopic(issuer, requiredClaims[claimTopic])
+                    ) {
+                        j = claimIds.length;
+                    }
+                    if (!hasClaimTopic(issuer, requiredClaims[claimTopic]) && j == (claimIds.length - 1)) {
+                        return false;
+                    }
+                    if (!_validity && j == (claimIds.length - 1)) {
+                        return false;
+                    }
+                }
+                catch {
+                    if (j == (claimIds.length - 1)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+
+}
---
+++ factory/IdFactory.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "ONCHAINID/interface/IImplementationAuthority.sol";
+import "ONCHAINID/proxy/IdentityProxy.sol";
+import "./IIdFactory.sol";
+
+contract IdFactory is IIdFactory {
+
+    /// event emitted whenever a single contract is deployed by the factory
+    event Deployed(address _addr);
+
+    event WalletLinked(address wallet, address identity);
+    event WalletUnlinked(address wallet, address identity);
+
+    /// address of the implementationAuthority contract making the link to the implementation contract
+    address implementationAuthority;
+
+    /// as it is not possible to deploy 2 times the same contract address, this mapping allows us to check which
+    /// salt is taken and which is not
+    mapping(string => bool) saltTaken;
+
+    mapping(address => address) userIdentity;
+
+
+    /// setting
+    constructor (address _implementationAuthority) {
+        implementationAuthority = _implementationAuthority;
+    }
+
+    /// deploy function with create2 opcode call
+    /// returns the address of the contract created
+    function deploy(string memory salt, bytes memory bytecode) internal returns (address) {
+        bytes memory implInitCode = bytecode;
+        address addr;
+        assembly {
+            let encoded_data := add(0x20, implInitCode) // load initialization code.
+            let encoded_size := mload(implInitCode)     // load init code's length.
+            addr := create2(0, encoded_data, encoded_size, salt)
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+        emit Deployed(addr);
+        return addr;
+    }
+
+    /// function used to deploy an identity using CREATE2
+    function deployIdentity
+    (
+        string memory _salt,
+        address _implementationAuthority,
+        address _wallet
+    ) internal returns (address){
+        bytes memory _code = type(IdentityProxy).creationCode;
+        bytes memory _constructData = abi.encode(_implementationAuthority, _wallet);
+        bytes memory bytecode = abi.encodePacked(_code, _constructData);
+        return deploy(_salt, bytecode);
+    }
+
+
+
+    /// function used to create a new identity contract
+    /// the function is deploying a proxy contract linked to the implementation contract
+    /// previously deployed and is taking a string as salt to deploy the ID
+    function createIdentity(address _wallet, string memory _salt) public override returns (address) {
+        require (!saltTaken[_salt], "salt already taken");
+        require (userIdentity[_wallet] == address(0), "wallet already linked to an identity");
+        address identity = deployIdentity(_salt, implementationAuthority, _wallet);
+        saltTaken[_salt] = true;
+        userIdentity[_wallet] = identity;
+        emit WalletLinked(_wallet, identity);
+        return identity;
+    }
+
+    /// function used to link a new wallet to an existing identity contract
+    /// function has to be called by the ONCHAINID owner, i.e. a wallet already linked to the contract
+    function linkWallet(address _newWallet) public override {
+        require(userIdentity[msg.sender] != address(0), "wallet not linked to an identity contract");
+        address identity = userIdentity[msg.sender];
+        userIdentity[_newWallet] = identity;
+        emit WalletLinked(_newWallet, identity);
+    }
+
+    /// function used to unlink a wallet from an identity contract
+    /// function has to be called by the ONCHAINID owner, i.e. a wallet already linked to the contract
+    /// cannot be called on msg.sender address to ensure there is always minimum 1 wallet linked to an ONCHAINID
+    function unlinkWallet(address _oldWallet) public override {
+        require(_oldWallet != msg.sender, "cannot be called on sender address");
+        require(userIdentity[msg.sender] == userIdentity[_oldWallet], "only a linked wallet can unlink");
+        address _identity = userIdentity[_oldWallet];
+        delete userIdentity[_oldWallet];
+        emit WalletUnlinked(_oldWallet, _identity);
+    }
+
+    /// getter function that returns the ONCHAINID contract address linked to a wallet
+    function getIdentity(address _wallet) public override view returns (address) {
+        return userIdentity[_wallet];
+    }
+}
